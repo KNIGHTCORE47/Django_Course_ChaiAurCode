df72cb90d94817fdfaf09bf2cbb388b3fd0156a0
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-12-09 00:37:50 +0530

    Refactor Home View to Render HTML Template Instead of Plain Text Response
    - Updated the `home` view in `views.py` to render an HTML template instead of returning a plain text HTTP response.
    - Changed the implementation from:
      ```python
      return HttpResponse("Hello, world. You're at the home page.")

--- 002_Django_Project_01_Templates_and_Errors/djangoProject01/djangoProject01/views.py
+++ 002_Django_Project_01_Templates_and_Errors/djangoProject01/djangoProject01/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+    return render(request, 'html/index.html')
 
 def about(request):
     return HttpResponse("Hello, world. You're at the about page.")
