ab04d626aab9597a05d59c84f6ebb4cb99d512ea
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-12-08 22:17:45 +0530

    Implement Basic Views for Home, About, and Contact Pages in Django Project
    - Added a new `views.py` file to the 'djangoProject01' application, defining three essential views:
      - `home`: Returns a simple HTTP response with a greeting message for the home page.
      - `about`: Returns a simple HTTP response with a greeting message for the about page.
      - `contact`: Returns a simple HTTP response with a greeting message for the contact page.
    - These views serve as the foundational building blocks for the web application, allowing for further development of templates and dynamic content in future commits.
    - The implementation of these views enhances the user experience by providing distinct endpoints for key sections of the application, setting the stage for more complex functionality and template rendering.

---
+++ 002_Django_Project_01_Templates_and_Errors/djangoProject01/djangoProject01/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the home page.")
+
+def about(request):
+    return HttpResponse("Hello, world. You're at the about page.")
+
+def contact(request):
+    return HttpResponse("Hello, world. You're at the contact page.")
