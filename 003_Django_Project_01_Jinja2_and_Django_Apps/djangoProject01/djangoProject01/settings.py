cb844b5a4a8b115d9133ae48ff35d1844c0d598d
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-12-10 00:53:10 +0530

    feat: Add 'djangoInnerApp' to INSTALLED_APPS in settings.py
    - Included the new application 'djangoInnerApp' in the `INSTALLED_APPS` list within the Django project settings. This integration allows the application to be recognized and utilized by the Django framework.
    - Added a comment to clarify the addition of 'djangoInnerApp', providing context for future developers or maintainers regarding the purpose of this change.
    
    This commit is a crucial step in expanding the functionality of the project by incorporating the new inner application, enabling its features and capabilities within the overall Django ecosystem.

--- 003_Django_Project_01_Jinja2_and_Django_Apps/djangoProject01/djangoProject01/settings.py
+++ 003_Django_Project_01_Jinja2_and_Django_Apps/djangoProject01/djangoProject01/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoInnerApp',    # NOTE - here we have added django inner app
 ]
 
 MIDDLEWARE = [
